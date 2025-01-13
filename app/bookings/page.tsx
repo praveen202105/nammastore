5990bc20f5bcc07b1937e75e071c6b64ec360c6b
Praveen Gupta <praveengupta@Praveens-MacBook-Air.local>
2025-01-13 18:52:08 +0530

    Renamed Signin to signin

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -50,7 +50,7 @@ export default function MyOrdersPage() {
         }
         console.log("tokenn ", token);
 
-        const response = await fetch("http://localhost:3000/api/orders/get", {
+        const response = await fetch("/api/orders/get", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
